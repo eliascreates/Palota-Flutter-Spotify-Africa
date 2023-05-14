e77bc8cbab161e61ca1d8f2d1d882aac93308f98
Elias Kekana <thapeloelias.kekana@gmail.com>
2023-05-14 20:33:41 +0200

    Add spotify package and display basic spotify api data on the screen

--- lib/features/spotify/presentation/pages/spotify_category.dart
+++ lib/features/spotify/presentation/pages/spotify_category.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spotify_africa_assessment/colors.dart';
+import 'package:flutter_spotify_africa_assessment/features/spotify/models/spotify_service.dart';
 import 'package:flutter_spotify_africa_assessment/routes.dart';
+import 'package:spotify/spotify.dart' as api;
 
 // TODO: fetch and populate playlist info and allow for click-through to detail
 // Feel free to change this to a stateful widget if necessary
-class SpotifyCategory extends StatelessWidget {
+
+class SpotifyCategory extends StatefulWidget {
   final String categoryId;
 
   const SpotifyCategory({
@@ -13,10 +16,26 @@ class SpotifyCategory extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<SpotifyCategory> createState() => _SpotifyCategoryState();
+}
+
+class _SpotifyCategoryState extends State<SpotifyCategory> {
+  late Future<Iterable<api.PlaylistSimple>?> userPlaylist;
+
+  @override
+  void initState() {
+    super.initState();
+    userPlaylist = SpotifyService.fetchPlaylist(widget.categoryId);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("{CategoryName}"),
+        centerTitle: true,
+        title: Text(
+          widget.categoryId[0].toUpperCase() + widget.categoryId.substring(1),
+        ),
         actions: [
           IconButton(
             icon: const Icon(Icons.info_outline),
@@ -40,9 +59,38 @@ class SpotifyCategory extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.all(16),
         child: Center(
-          child: Text(
-            '''Populate with playlist info for category '$categoryId', click on playlist to view playlist detail''',
-            textAlign: TextAlign.center,
+          child: FutureBuilder(
+            future: userPlaylist,
+            builder: (context, snapshot) {
+              debugPrint("the snapshot $snapshot");
+              if (snapshot.hasData) {
+                return GridView.builder(
+                  itemCount: snapshot.data!.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Text(snapshot.data!.elementAt(index).name!),
+                        SizedBox(
+                          width: 100,
+                          child: Image.network(
+                              fit: BoxFit.cover,
+                              snapshot.data!
+                                  .elementAt(index)
+                                  .images!
+                                  .first
+                                  .url!),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.hasError}');
+              }
+              return const CircularProgressIndicator();
+            },
           ),
         ),
       ),
